fa18a0f4bf7d4e13f7a3b0e3a36f2800e8b415f1
Suresh Sehrawat <jaisehrawat11@gmail.com>
2024-08-12 13:12:37 +0530

    backend init

---
+++ illusion-backend/src/main/java/org/boldbit/illusionbackend/IllusionBackendApplication.java
@@ -0,0 +1,13 @@
+package org.boldbit.illusionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllusionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IllusionBackendApplication.class, args);
+    }
+
+}
---
+++ illusion-backend/src/test/java/org/boldbit/illusionbackend/IllusionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boldbit.illusionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IllusionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
