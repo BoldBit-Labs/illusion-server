483fcaef26c251d269dfb39600c4038bab4f5a15
Suresh Sehrawat <jaisehrawat11@gmail.com>
2024-10-02 01:15:00 +0530

    cors allowed url changed

--- illusion-backend/user-service/src/main/java/org/boldbit/illusionbackend/userservice/config/CorsConfig.java
+++ illusion-backend/user-service/src/main/java/org/boldbit/illusionbackend/userservice/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://illusion.com:3333") // allow only this origin
+                        .allowedOrigins("https://illusion-server.buzz") // allow only this origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
